50ad096c12e1ec0f9095746c988d2b0f749c0965
Pedro <pedrokarnoski@gmail.com>
2024-10-30 16:28:37 -0300

    feat: Criar evento na agenda pessoal

--- src/components/new-schedule.tsx
+++ src/components/new-schedule.tsx
@@ -1,4 +1,5 @@
 import { useMutation, useQuery } from '@tanstack/react-query'
+import { CalendarEvent, google, office365, outlook, yahoo } from 'calendar-link'
 import { format, isBefore, isToday } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { Calendar as CalendarIcon, Check, Clock } from 'lucide-react'
@@ -7,6 +8,15 @@ import { useState } from 'react'
 import { getUser } from '@/api/get-user'
 import { registerSchedule } from '@/api/register-schedule'
 import { TimeSlots } from '@/components/times-slots'
+import {
+  AlertDialog,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from '@/components/ui/alert-dialog'
 import { Button } from '@/components/ui/button'
 import { Calendar } from '@/components/ui/calendar'
 import { Input } from '@/components/ui/input'
@@ -16,6 +26,7 @@ import {
   PopoverContent,
   PopoverTrigger,
 } from '@/components/ui/popover'
+import { ToastAction } from '@/components/ui/toast'
 import { toast } from '@/components/ui/use-toast'
 import { queryClient } from '@/lib/react-query'
 import { axiosErrorHandler } from '@/utils/axiosErrorHandler'
@@ -74,19 +85,26 @@ export function NewSchedule() {
     onSuccess: async () => {
       await queryClient.invalidateQueries({ queryKey: ['schedules'] })
 
-      setDate(undefined)
-      setHour(null)
-
       toast({
         variant: 'default',
         title: 'Agendamento',
-        description: 'Agendado realizado!',
+        description: 'Agendamento realizado!',
+        action: (
+          <ToastAction altText="Fazer login">
+            Adicionar ao calendário
+          </ToastAction>
+        ),
+        onClick: () => setIsOpenAlertDialog(true),
       })
     },
   })
 
   const [date, setDate] = useState<Date>()
   const [hour, setHour] = useState<string | null>(null)
+  const [isOpenAlertDialog, setIsOpenAlertDialog] = useState<boolean>(false)
+
+  const dateHour =
+    date && hour ? `${format(date, 'yyyy-MM-dd')}T${hour}:00` : ''
 
   async function handleCreateNewSchedule() {
     try {
@@ -97,10 +115,6 @@ export function NewSchedule() {
           description: 'Preencha todos os campos para agendar.',
         })
       }
-
-      const dateHour = date
-        ? format(date, 'yyyy-MM-dd') + 'T' + hour + ':00'
-        : ''
 
       await registerScheduleFn({
         patient: user?.patient,
@@ -117,8 +131,58 @@ export function NewSchedule() {
     }
   }
 
+  const event: CalendarEvent = {
+    title: 'Consulta médica',
+    description: 'Agendamento de consulta médica - Saúde Online',
+    start: `${dateHour.replace('T', ' ')} +0300`,
+    duration: [30, 'minutes'],
+  }
+
+  const googleUrl = google(event)
+  const outlookUrl = outlook(event)
+  const office365Url = office365(event)
+  const yahooUrl = yahoo(event)
+
+  const handleOpenCalendar = (url: string) => {
+    window.open(url, '_blank')
+    setIsOpenAlertDialog(false)
+
+    setDate(undefined)
+    setHour(null)
+  }
+
   return (
     <div className="flex items-center">
+      <AlertDialog open={isOpenAlertDialog}>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>Adicionar ao calendário pessoal</AlertDialogTitle>
+            <AlertDialogDescription>
+              Escolha o calendário que deseja adicionar o agendamento
+            </AlertDialogDescription>
+            <div className="flex items-center justify-center gap-2 pt-4">
+              <Button size="sm" onClick={() => handleOpenCalendar(googleUrl)}>
+                Google Calendar
+              </Button>
+              <Button size="sm" onClick={() => handleOpenCalendar(outlookUrl)}>
+                Outlook
+              </Button>
+              <Button
+                size="sm"
+                onClick={() => handleOpenCalendar(office365Url)}
+              >
+                Office 365
+              </Button>
+              <Button size="sm" onClick={() => handleOpenCalendar(yahooUrl)}>
+                Yahoo Calendar
+              </Button>
+            </div>
+          </AlertDialogHeader>
+          <AlertDialogFooter>
+            <AlertDialogCancel>Cancelar</AlertDialogCancel>
+          </AlertDialogFooter>
+        </AlertDialogContent>
+      </AlertDialog>
       <div className="flex-col justify-between rounded-lg p-10 md:flex">
         <div className="space-y-4">
           <div className="flex flex-row items-center gap-3">
