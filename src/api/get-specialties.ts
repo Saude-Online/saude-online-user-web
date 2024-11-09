93fb7c366603632bf863a66bb6a77df58e660003
Pedro Henrique Karnoski <pedrokarnoski@gmail.com>
2024-11-09 17:49:46 -0300

    feat: Filtrar médicos por especialidade

---
+++ src/api/get-specialties.ts
@@ -0,0 +1,14 @@
+import { api } from '@/lib/axios'
+
+export interface GetSpecialtiesResponse {
+  specialties: {
+    id: string
+    name: string
+  }
+}
+
+export async function getSpecialties() {
+  const response = await api.get<GetSpecialtiesResponse>('/specialties')
+
+  return response.data.specialties
+}
